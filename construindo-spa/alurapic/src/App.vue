a0a510c9e85b5c98d4d462c50ab8ccb3190106b9
Otávio Borges <otavio.demb@rede.ulbra.br>
2022-03-31 21:54:30 -0300

    firstPost

---
+++ construindo-spa/alurapic/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+
+  <div class="corpo">
+    <router-view></router-view>
+  </div>
+
+</template>
+
+
+
+<script>
+
+export default {
+
+}
+</script>
+
+
+
+<style>
+  .corpo{
+    font-family: Helvetica;
+    width: 96%;
+    margin: 0 auto;
+  }
+
+  
+</style>
